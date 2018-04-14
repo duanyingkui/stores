a354356fa8da14a3da928de9eff1d774789116d1
gaocuili <1657150419@qq.com>
2018-04-15 07:40:09 +0800

    customer

--- app/Http/Controllers/Admin/CustomerController.php
+++ app/Http/Controllers/Admin/CustomerController.php
@@ -20,7 +20,7 @@ class CustomerController{
 
         $pageSize = $request->input('pageSize', 5);
         $address  = Customer::join('address','customer.id','=','address.customer_id')
-            ->select('customer.id','customer.name','customer.linkman','address_name','customer.phone')
+            ->select('customer.id','customer.name','customer.linkman','address_name','customer.phone','customer.created_at')
             ->where('address.status',0)
             ->paginate($pageSize);
         Log::info(json_encode($address));
@@ -35,96 +35,82 @@ class CustomerController{
         $name         = $request->input('name','');
         $linkman      = $request->input('linkman','');
         $phone        = $request->input('phone','');
-        $address_n      = $request->input('address','');
+        $address_n    = $request->input('address','');
         $code         = implode('/',$request->input('code'));
-
         $customer   = new Customer;
         $address    = new Address;
         $user       = new User;
+        $salt_data  = get_salt();
         DB::beginTransaction();
 
-//          try{
-              $customer_id = $customer->insertGetId(
-                  ['name' => $name , 'linkman' => $linkman , 'phone' => $phone]
-              );
+         try{
+              
+              $customer->name             = $name;
+              $customer->linkman          = $linkman;
+              $customer->phone            = $phone;
+              $customer->save();
 
-              $address_data = $address -> insert(
-                  ['consignee' => $linkman , 'consignee_mobile' => $phone, 'address_name' => $address_n ,
-                      'code' => $code , 'status' => 0 , 'customer_id' => $customer_id]
-              );
+              $address->consignee         = $linkman;
+              $address->consignee_mobile  = $phone;
+              $address->address_name      = $address_n;
+              $address->code              = $code;
+              $address->status            = 0;
+              $address->customer_id       = $customer->id;
+              $address->save();
 
-              $user_data = $user -> insert(
-                  ['name' => $linkman , 'phone' => $phone , 'password' => '666' , 'salt' => '3ND5' ,'status' => 0 , 'type' => 0]
-              );
+              $user->name     = $phone;
+              $user->phone    = $phone;
+              $user->password = encrypt_password("123",$salt_data);
+              $user->salt     = $salt_data;
+              $user->status   = 0;
+              $user->type     = 2;
+              $user->save();
 
-            if($customer_id && $address_data && $user_data){
-                DB::commit();
-                return Response::json(['code' => 0 , 'msg' => '操作成功']);
-            }else{
-                DB::rollback();
-                return Response::json(['code' => 1 , 'msg' => '操作失败']);
-            }
+              DB::commit();
+              return Response::json(['code' => 0 , 'msg' => '操作成功']);
+          }catch (Exception $err){
+              DB::rollback();
+              Log::info($err);
+              return Response::json(['code' => 1 , 'msg' => '操作失败']);
+          }
 
-//              $address->consignee         = $linkman;
-//              $address->consignee_mobile  = $phone;
-//              $address->address_name      = $address_n;
-//              $address->code              = $code;
-//              $address->status            = 0;
-//              $address->customer_id       = $customer_id;
-//              $address-save();
-//
-//              $user->name     = $linkman;
-//              $user->phone    = $phone;
-//              $user->password = 1;
-//              $user->salt     = "3ND5";
-//              $user->status   = 1;
-//              $user-save();
-
-//              DB::commit();
-//              return Response::json(['code' => 0 , 'msg' => '操作成功']);
-//          }catch (Exception $err){
-//              DB::rollback();
-//              Log::info($err);
-//              return Response::json(['code' => 1 , 'msg' => '操作失败']);
-//          }
   }
 
-  /*
-    根据制定ID查询客户信息
-  */
+    /*
+     * 根据指定ID查询客户信息->前台修改
+     * @param Request $request
+     */
   function getCustomer(Request $request){
-      $customer = $request->input('id');
+      $customer_id    = $request->input('id');
+      $customer_data  = Customer::find($customer_id);
+      $address_data   = Address::select('address_name','code')
+            ->where('customer_id',$customer_id)->get();
+      return Response::json(['customer' => $customer_data , 'address' => $address_data]);
+  }
 
 
+  /**
+   * 根据指定ID删除客户信息->前台修改
+   * @param Request $request
+   */
+  function delCustomer(Request $request){
+    $customer_id  = $request->input('id');
+    $user         = $request->input('phone');
+    DB::beginTransaction();
+    $delCustomer  = Customer::where('id',$customer_id)
+                              ->delete();
+    $delAddress  = Address::where('customer_id',$customer_id)
+                            ->delete();
+    $delUser = User::where('name',$user)
+                    ->update(['status'=>1]);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    if($delCustomer && $delAddress && $delUser){
+        DB::commit();
+        return Response::json(['code' => 0 , 'msg' => '操作成功']);
+    }else{
+        DB::rollback();
+        return Response::json(['code' => 1 , 'msg' => '操作失败' , 'delCustomer' => $delCustomer , 'delAddress' => $delAddress , 'delUser' => $delUser]);
+    }
 
 
   }
