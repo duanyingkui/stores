4ab7a8f046a56bc47d8fcac6e6a68b87ddb6eaba
gaocuili <1657150419@qq.com>
2018-04-19 15:34:45 +0800

    customer

--- app/Http/Controllers/Admin/CustomerController.php
+++ app/Http/Controllers/Admin/CustomerController.php
@@ -1,12 +1,13 @@
 <?php 
- 
+ /**
+ * Created by Sublime.
+ * User: gaocuili
+ */   
+
   namespace App\Http\Controllers\Admin;
 
   use App\Models\Admin\Customer;
-  use App\Models\Admin\Address;
-  use App\Models\Admin\User;
   use Illuminate\Http\Request;
-  use Illuminate\Support\Facades\DB;
   use Response;
   use Log;
   use Redirect;
@@ -20,24 +21,7 @@ class CustomerController{
 
         $pageSize   = $request->input('pageSize', 5);
         $queryData  = $request->input('queryData');
-        if($queryData){
-          $address  = Customer::join('address','customer.id','=','address.customer_id')
-            ->select('customer.id','customer.name','customer.linkman','address_name','customer.phone','customer.created_at')
-            ->where('address.status',0)
-            ->paginate($pageSize);
-        }
-        $address  = Customer::join('address','customer.id','=','address.customer_id')
-            ->select('customer.id','customer.name','customer.linkman','address_name','customer.phone','customer.created_at')
-            ->where([
-              ['address.status','=', 0],
-              ['customer.name','like','%'.$queryData.'%'],
-            ])
-            ->orWhere([
-              ['address.status','=', 0],
-              ['customer.linkman','like','%'.$queryData.'%'],
-            ])
-            ->paginate($pageSize);
-        Log::info(json_encode($address));
+        $address = Customer::getCustomer($queryData,$pageSize);
         return Response::json(['customer' => $address]);
     }
 
@@ -51,54 +35,21 @@ class CustomerController{
         $phone        = $request->input('phone','');
         $address_n    = $request->input('address','');
         $code         = implode('/',$request->input('code'));
-        $customer   = new Customer;
-        $address    = new Address;
-        $user       = new User;
-        $salt_data  = get_salt();
-        DB::beginTransaction();
-         try{
-
-              $customer->name             = $name;
-              $customer->linkman          = $linkman;
-              $customer->phone            = $phone;
-              $customer->save();
-
-              $address->consignee         = $linkman;
-              $address->consignee_mobile  = $phone;
-              $address->address_name      = $address_n;
-              $address->code              = $code;
-              $address->status            = 0;
-              $address->customer_id       = $customer->id;
-              $address->save();
-
-              $user->name     = $phone;
-              $user->phone    = $phone;
-              $user->password = encrypt_password("123456",$salt_data);
-              $user->salt     = $salt_data;
-              $user->status   = 0;
-              $user->type     = 2;
-              $user->save();
-
-              DB::commit();
-              return Response::json(['code' => 0 , 'msg' => '操作成功']);
-          }catch (Exception $err){
-              DB::rollback();
-              Log::info($err);
-              return Response::json(['code' => 1 , 'msg' => '操作失败']);
-          }
+        $addCustomer  = Customer::addCustomer($name,$linkman,$phone,$address_n,$code);
+        return Response::json(["addCustomer" => $addCustomer]);        
   }
 
-    /*
-     * 根据指定ID查询客户信息->前台修改
-     * @param Request $request
-     */
-  function getCustomer(Request $request){
-      $customer_id    = $request->input('id');
-      $customer_data  = Customer::find($customer_id);
-      $address_data   = Address::select('address_name','code')
-            ->where('customer_id',$customer_id)->get();
-      return Response::json(['customer' => $customer_data , 'address' => $address_data]);
-  }
+  //   /*
+  //    * 根据指定ID查询客户信息->前台修改
+  //    * @param Request $request
+  //    */
+  // function getCustomer(Request $request){
+  //     $customer_id    = $request->input('id');
+  //     $customer_data  = Customer::find($customer_id);
+  //     $address_data   = Address::select('address_name','code')
+  //           ->where('customer_id',$customer_id)->get();
+  //     return Response::json(['customer' => $customer_data , 'address' => $address_data]);
+  // }
 
 
   /**
@@ -108,22 +59,9 @@ class CustomerController{
   function delCustomer(Request $request){
     $customer_id  = $request->input('id');
     $user         = $request->input('phone');
-    DB::beginTransaction();
-    $delCustomer  = Customer::where('id',$customer_id)
-                              ->delete();
-    $delAddress  = Address::where('customer_id',$customer_id)
-                            ->delete();
-    $delUser = User::where('name',$user)
-                    ->update(['status'=>1]);
-
-    if($delCustomer && $delAddress && $delUser){
-        DB::commit();
-        return Response::json(['code' => 0 , 'msg' => '操作成功']);
-    }else{
-        DB::rollback();
-        return Response::json(['code' => 1 , 'msg' => '操作失败' , 'delCustomer' => $delCustomer , 'delAddress' => $delAddress , 'delUser' => $delUser]);
-    }
-
-
+    $delCustomer  = Customer::delCustomer($customer_id,$user);
+    return Response::json(["delCustomer" => $delCustomer]);        
   }
+
+//TODO:: 批量删除后台
 }
--- app/Models/Admin/Customer.php
+++ app/Models/Admin/Customer.php
@@ -8,17 +8,106 @@
 
 namespace App\Models\Admin;
 
-
 use Illuminate\Database\Eloquent\Model;
+use Log;
+use App\Models\Admin\Address;
+use App\Models\Admin\User;
+use App\Models\Admin\Customer;
+use Illuminate\Support\Facades\DB;
+use Response;
 
 class Customer extends Model
 {
     protected $table = 'customer';
     protected $fillable = ['id','name','linkman','phone'];
+
     public function addresses(){
         return $this->hasMany('App\Models\Admin\Address');
     }
+
     public function fromDateTime($value){
         return strtotime(parent::fromDateTime($value));
     }
+
+    public static function getCustomer($queryData,$pageSize){
+    	if($queryData){
+          $address = Customer::join('address','customer.id','=','address.customer_id')
+            ->select('customer.id','customer.name','customer.linkman','address_name','customer.phone','customer.created_at')
+            ->where('address.status',0)
+            ->paginate($pageSize);
+        }
+        $address  = Customer::join('address','customer.id','=','address.customer_id')
+            ->select('customer.id','customer.name','customer.linkman','address_name','customer.phone','customer.created_at')
+            ->where([
+              ['address.status','=', 0],
+              ['customer.name','like','%'.$queryData.'%'],
+            ])
+            ->orWhere([
+              ['address.status','=', 0],
+              ['customer.linkman','like','%'.$queryData.'%'],
+            ])
+            ->paginate($pageSize);
+        Log::info(json_encode($address));
+        return $address;
+    }
+
+    public static function addCustomer($name,$linkman,$phone,$address_n,$code){
+    	$customer   = new Customer;
+        $address    = new Address;
+        $user       = new User;
+        $salt_data  = get_salt();
+        DB::beginTransaction();
+         try{
+
+              $customer->name             = $name;
+              $customer->linkman          = $linkman;
+              $customer->phone            = $phone;
+              $customer->save();
+
+              $address->consignee         = $linkman;
+              $address->consignee_mobile  = $phone;
+              $address->address_name      = $address_n;
+              $address->code              = $code;
+              $address->status            = 0;
+              $address->customer_id       = $customer->id;
+              $address->save();
+
+              $user->name     = $phone;
+              $user->phone    = $phone;
+              $user->password = encrypt_password("123456",$salt_data);
+              $user->salt     = $salt_data;
+              $user->status   = 0;
+              $user->type     = 2;
+              $user->save();
+
+              DB::commit();
+              return Response::json(['code' => 0 , 'msg' => '操作成功']);
+          }catch (Exception $err){
+              DB::rollback();
+              Log::info($err);
+              return Response::json(['code' => 1 , 'msg' => '操作失败']);
+          }
+    }
+
+    public static function delCustomer($customer_id,$user){
+    	DB::beginTransaction();
+	    $delCustomer  = Customer::where('id',$customer_id)
+	                              ->delete();
+	    $delAddress  = Address::where('customer_id',$customer_id)
+	                            ->delete();
+	    $delUser = User::where('name',$user)
+	                    ->update(['status'=>1]);
+
+	    if($delCustomer && $delAddress && $delUser){
+	        DB::commit();
+	        return Response::json(['code' => 0 , 'msg' => '操作成功']);
+	    }else{
+	        DB::rollback();
+	        return Response::json(['code' => 1 , 'msg' => '操作失败']);
+	    }
+    }
+
+
+
+
 }
