043ebbb6fe0ca2e91a4776249cc9f413e551c7e2
weiyalin <727299708@qq.com>
2018-05-10 11:37:26 +0800

    '客户列表问题解决'

--- resources/assets/js/components/admin/customer/customerList.vue
+++ resources/assets/js/components/admin/customer/customerList.vue
@@ -51,8 +51,7 @@
                 @size-change="onPageSizeChange"
                 @current-change="onPageChange"
                 :current-page="page"
-                :
-                ="[5, 10, 20, 50, 100]"
+                page-sizes:="[5, 10, 20, 50, 100]"
                 :page-size="pageSize"
                 layout="total, sizes, prev, pager, next, jumper"
                 :total="total">
